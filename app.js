d7dfd9a3ff0de974e0cdacae7a0bc13f5c11ab0b
jonnatan053-ship-it <jonnatan053@gmail.com>
2026-09-08 00:45:58 -0300

    fix: alterei a logica da conta do valaor

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function calculartotal (itens) {
     let total = 0;
 
     for (let i = 0; i < itens.length; i++) {
-        total += itens[i].preco * itens[i].quantidade * 1.1;
+        total += itens[i].preco * itens[i].quantidade 
     }
     
 } return total;
