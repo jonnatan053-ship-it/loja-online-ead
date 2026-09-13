39c04bb1a56bab65bd8631d6e8abfbe22d5ec746
jonnatan053-ship-it <jonnatan053@gmail.com>
2026-09-13 15:52:48 -0300

    fix: alteracap total do app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ app.js
 
 
 function calculartotal (itens) {
-    let total = 0;
+    let total = 1
+
 
     for (let i = 0; i < itens.length; i++) {
         total += itens[i].preco * itens[i].quantidade 
